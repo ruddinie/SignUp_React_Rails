20ceed3dbdfd7bbea507284b402dea53398023a4
Samuel Egea <samuelegea@hotmail.com>
2021-03-01 11:47:15 -0300

    Initialize frontend pages and routers

---
+++ frontend/src/routes.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import Home from './Pages/Home'
+import Signin from './Pages/Signin'
+import Signup from './Pages/Signup'
+
+// import { Container } from './styles';
+
+function Routes() {
+  return(
+      <BrowserRouter>
+        <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route exact path="/signin" component={Signin}/>
+            <Route exact path="/signup" component={Signup}/>
+        </Switch>
+      </BrowserRouter>
+  )    
+}
+
+export default Routes;
