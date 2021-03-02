9dbde7aad22b7b1f47573c61c7361815b54bcee8
Samuel Egea <samuelegea@hotmail.com>
2021-03-02 09:16:03 -0300

    add signup form

--- frontend/src/Pages/Signup.js
+++ frontend/src/Pages/Signup.js
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Button, Form, Jumbotron } from 'react-bootstrap'
+import { Container }  from './styles.js'
 
-function Signup(props) {
+  
+function Signup() {
+    const [data, setData] = useState({
+        email:'',
+        password:'',
+    })
+
     return (
-        <div>
-            Signup
-        </div>
+        <Jumbotron>
+            <Container>
+                <div className="form">
+                    <h3>Cadastre-se</h3>
+                    <br />
+                    <Form>
+                        <Form.Group>
+                            <Form.Label>E-mail</Form.Label>
+                            <Form.Control type="email" placeholder="Seu melhor E-mail" value={data.email} onChange={e=> setData({...data, email: e.target.value})}/>
+                        </Form.Group>
+
+                        <Form.Group>
+                            <Form.Label>Senha</Form.Label>
+                            <Form.Control type="password" placeholder="Sua senha" value={data.password} onChange={e=> setData({...data, password: e.target.value})}/>
+                        </Form.Group>
+
+
+                        <Button variant="success" type="submit">
+                            Entrar
+                        </Button>
+                    </Form>
+                </div>
+            </Container>
+        </Jumbotron>
     );
 }
 
---
+++ frontend/src/Pages/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  height: 100vh;
+  .form{
+      max-width: 30em;
+      margin: auto;
+      padding: 3em;
+      background-color: #fafafa;
+      border-radius: 1.5em;
+      border: solid #bebebe 2px;
+      
+  }
+`;
+// export default Container;
