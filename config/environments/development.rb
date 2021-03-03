e3725ba668d7e4ed8f2ea380b3ea403d8aee7a3d
Samuel Egea <samuelegea@hotmail.com>
2021-03-03 20:00:57 -0300

    Configuring email delivery

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,4 +54,19 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    domain: "gmail.com",
+    port: 587,
+    user_name: "appemailtester21@gmail.com",
+    password: "qwerty123@",
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+  config.action_mailer.default_url_options = { :host => 'localhost:3000', protocol: 'http' }
 end
