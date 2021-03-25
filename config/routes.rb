6079f3ac52f5eb7b1c0a7f5aa1a465c4accb976c
samuelegea <samuelegea@hotmail.com>
2021-03-25 15:41:06 -0300

    Handle routes for SPA react app

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
 
   get '/dashboard/home', to: 'dashboard#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '*path', to: "application#fallback_index_html", constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
 end
