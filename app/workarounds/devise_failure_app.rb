cdc0b16865380af65e703f8f7db318c3c905b4cc
samuelegea <samuelegea@hotmail.com>
2021-03-25 19:19:24 -0300

    fix devise_failure_app issue

--- app/workarounds/devise_failure_app.rb
+++ app/workarounds/devise_failure_app.rb
@@ -1,9 +1,7 @@
-class MyApplicationFailureApp < Devise::FailureApp
-    def http_auth_body
-      return super unless request_format == :json
-      {
-        sucess: false,
-        message: i18n_message
-      }.to_json
-    end
-  end
+class CustomAuthFailure < Devise::FailureApp
+  def respond
+    self.status = 401 
+    self.content_type = 'json'
+    self.response_body = {"errors" => ["Invalid login credentials"]}.to_json
+  end 
+end
