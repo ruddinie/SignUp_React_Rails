10423b5412920cfb0ca3ef13126a4e53a83c0029
samuelegea <samuelegea@hotmail.com>
2021-03-25 20:07:01 -0300

    staging back previous commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::API
     before_action :configure_permitted_parameters, if: :devise_controller?
 
     def fallback_index_html
-      render :file => 'frontend/public/index.html'
+      render :file => 'public/index.html'
     end
 
 
